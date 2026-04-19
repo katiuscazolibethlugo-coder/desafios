206e1ce6264b880150fd30b711c1cd4f21dec27f
katiuscazolibethlugo <katiuscazolibethlugo@gmail.com>
2026-04-18 20:00:54 -0400

    Creando desafio

---
+++ desafiosSeccionB/desafiosSeccionB/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: noral
+ * Fecha: 18/4/2026
+ * Hora: 7:53 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace desafiosSeccionB
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
